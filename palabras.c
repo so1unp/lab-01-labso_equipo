f49db707da4d0dcfc860fcd3e7acfa5ecfb05404
Matias <matiasmepullan@gmail.com>
2025-03-20 16:08:21 -0300

    revision primeros dos

--- palabras.c
+++ palabras.c
@@ -1,10 +1,33 @@
 #include <stdio.h>
+#include <signal.h>
 #include <stdlib.h>
+
+void handler()
+{
+    exit(0);
+}
 
 int main(int argc, char *argv[])
 {
-    // Agregar código aquí.
-
-    // Termina la ejecución del programa.
+    char c;
+    signal(SIGINT, handler);
+    while (1)
+    {
+        while ((c = getchar()) != EOF)
+        {
+            if (c == ' ' || c == '\n')
+            {
+                putchar('\n');
+            }
+            else
+            {
+                putchar(c);
+            }
+            if (c == '\n')
+            {
+                break;
+            }
+        }
+    }
     exit(EXIT_SUCCESS);
 }
--- sum.c
+++ sum.c
@@ -1,10 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int suma_total = 0;
+
+void suma_elemento(int elemento)
+{
+    suma_total += elemento;
+}
+
+void sumar(int argc, char *argv[])
+{
+    for (int i = 1; i < argc; i++)
+    {
+        suma_elemento(atoi(argv[i]));
+    }
+}
+
+void imprimir()
+{
+    printf("%d\n", suma_total);
+}
+
 int main(int argc, char *argv[])
 {
-    // Agregar código aquí.
-
-    // Termina la ejecución del programa.
+    if (argc > 1)
+    {
+        sumar(argc, argv);
+    }
+    else
+    {
+        int numero;
+        while (scanf("%d", &numero) == 1)
+        {
+            suma_elemento(numero);
+        }
+    }
+    imprimir();
     exit(EXIT_SUCCESS);
 }
